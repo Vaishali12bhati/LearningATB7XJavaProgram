0303ebfb748dc5cfc72a6471d751c4288e7fb04f
ADMIN <Vaishali12bhati@gmail.com>
2024-07-20 23:49:46 +0530

    14072024

---
+++ src/ex_14072024/Lab059.java
@@ -0,0 +1,14 @@
+package ex_14072024;
+//difference between = , ==
+//= is use as assignmnet operator
+// == used for comparision
+
+public class Lab059 {
+    public static void main(String[] args) {
+    int a = 10;
+    boolean b =(10==11);
+    boolean c = !(10==11);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ src/ex_14072024/Lab064.java
@@ -0,0 +1,17 @@
+package ex_14072024;
+
+public class Lab064 {
+    public static void main(String[] args) {
+        int course = 100;
+        float GST = 18.56F;
+        int total_price = course +(int)GST; //narrow explicit
+        System.out.println(total_price);
+
+
+
+
+
+
+
+    }
+}
