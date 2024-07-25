e85271718442a1d68765e5f75c11dd56e8988d66
ADMIN <Vaishali12bhati@gmail.com>
2024-07-26 03:41:02 +0530

    210720241homework

---
+++ src/ex_21072024/Lab157.java
@@ -0,0 +1,34 @@
+package ex_21072024;
+//Right Triangle Star Pattern
+//
+//*
+//**
+//***
+//****
+//*****
+public class Lab157 {
+    public static void main(String[] args) {
+        int num = 5;// no of line pattern
+        for (int i = 1; i < num; i++) {
+            //exp 1 i= 2
+            //exp =2 i =3
+            //exp =3 i = 4
+            for (int j = 1; j <i; j++) {
+                System.out.print("*");
+                //exp j 1, j= 2
+                //exp j = 2 , j =3
+                //exp j = 3 ,j = 4
+
+                //*
+                //**
+                //***
+
+
+
+            }
+                System.out.println();
+            }
+
+        }
+    }
+
---
+++ src/ex_21072024/Lab160.java
@@ -0,0 +1,8 @@
+package ex_21072024;
+
+public class Lab160 {
+    public static void main(String[] args) {
+
+    }
+
+}
